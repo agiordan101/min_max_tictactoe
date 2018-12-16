5b78f47b0caa58010b561ab920b6f5e74b2b02af
agiordan <39808296+agiordan101@users.noreply.github.com>
2018-12-16 23:27:31 +0100

    Fin morpion, plus que IA

--- morpion.c
+++ morpion.c
@@ -32,44 +32,102 @@ void	init(t_morpion *var)
 	}
 	var->grid[var->size] = NULL;
 	var->line = NULL;
-	var->choice = 6;
+	var->choice = 0;
 	var->game = 1;
 }
 
+void	ft_print_grid(t_morpion *var)
+{
+	ft_putchar(var->grid[0][0]);
+	ft_putchar('|');
+	ft_putchar(var->grid[0][1]);
+	ft_putchar('|');
+	ft_putchar(var->grid[0][2]);
+	ft_putstr("\n-----\n");
+	ft_putchar(var->grid[1][0]);
+	ft_putchar('|');
+	ft_putchar(var->grid[1][1]);
+	ft_putchar('|');
+	ft_putchar(var->grid[1][2]);
+	ft_putstr("\n-----\n");
+	ft_putchar(var->grid[2][0]);
+	ft_putchar('|');
+	ft_putchar(var->grid[2][1]);
+	ft_putchar('|');
+	ft_putchar(var->grid[2][2]);
+	ft_putstr("\n\n");
+}
+/*
+O|X|
+-----
+ | |
+-----
+ | |
+*/
+
 int	ft_end(t_morpion *var)
 {
-	if (var->grid[0][0] == var->grid[0][1] == var->grid[0][2])
+	if (var->grid[0][0] == var->grid[0][1] && var->grid[0][0] == var->grid[0][2] && var->grid[0][0] != ' ')
 		return (1);
-	return (0)
+	if (var->grid[1][0] == var->grid[1][1] && var->grid[1][0] == var->grid[1][2] && var->grid[1][0] != ' ')
+		return (1);
+	if (var->grid[2][0] == var->grid[2][1] && var->grid[2][0] == var->grid[2][2] && var->grid[2][0] != ' ')
+		return (1);
+	if (var->grid[0][0] == var->grid[1][0] && var->grid[0][0] == var->grid[2][0] && var->grid[0][0] != ' ')
+		return (1);
+	if (var->grid[0][1] == var->grid[1][1] && var->grid[0][1] == var->grid[2][1] && var->grid[0][1] != ' ')
+		return (1);
+	if (var->grid[0][2] == var->grid[1][2] && var->grid[0][2] == var->grid[2][2] && var->grid[0][2] != ' ')
+		return (1);
+	if (var->grid[0][0] == var->grid[1][1] && var->grid[0][0] == var->grid[2][2] && var->grid[0][0] != ' ')
+		return (1);
+	if (var->grid[0][2] == var->grid[1][1] && var->grid[0][2] == var->grid[2][0] && var->grid[0][2] != ' ')
+		return (1);
+	return (0);
 }
 
 void	ft_AI_turn(t_morpion *var)
 {
-
+	
 }
 
 void	morpion(t_morpion *var)
 {
-	//var->turn = 'O';
-	while (game)
+	int firsttry;
+
+	ft_print_grid(var);
+	while (!(var->game = ft_end(var)))
 	{
-		var->turn = (var->player == 'O' ? 'X' : 'O');
+		var->turn = (var->turn == 'O' ? 'X' : 'O');
 		if (var->turn == 'O')
 		{
-			while (var->choice >= 0 && var->choice <= 9 &&
-			var->grid[var->choice / 3][var->choice % 3] != ' ')
+			firsttry = 1;
+			while (firsttry == 1 ||
+			var->grid[(var->choice - 1) / 3]
+				[(var->choice - 1) % 3] != ' ')
 			{
-				ft_putstr("Make your choose : ");
-				get_next_line(1, &var->line);				//()
-				var->choice = ft_atoi(var->line);
+				firsttry = 1;
+				while (var->choice < 1 || var->choice > 9 || firsttry == 1)
+				{
+					ft_putstr("Make your choose : ");
+					firsttry = 0;
+					get_next_line(1, &var->line);				//()
+					var->choice = ft_atoi(var->line);
+				}
 			}
-			var->grid[var->choice / 3][var->choice % 3] = 'O';	
+			var->grid[(var->choice - 1) / 3][(var->choice - 1) % 3] = 'O';	
 		}
 		else
 		{
+			ft_AI_turn(var);
+			ft_putendl("AI has played :");
+			ft_print_grid(var);
 		}
-		game = ft_end(var);
 	}
+	if (var->turn == 'O')
+		ft_print_grid(var);
+	ft_putstr("Player win : ");
+	ft_putchar(var->turn);
 }
 
 int	main(int ac, char **av)
